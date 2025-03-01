9192dad2e16d7e517093ae5aba2ea7d2c819fab7
marina_serdyuk <serdyukmarina876@gmail.com>
2025-03-01 21:49:59 +0100

    Tasks 1-3 without css

---
+++ src/components/App/App.jsx
@@ -0,0 +1,34 @@
+import Profile from "../Profile/Profile";
+import userData from "/src/userData.json";
+import FriendList from "../FriendList/FriendList";
+import friends from "/src/friends.json"
+import TransactionHistory from "../TransactionHistory/TransactionHistory"
+import transactions from "/src/transactions.json"
+
+
+export default function App () {
+return (
+<>
+<Profile
+name={userData.username}
+tag={userData.tag}
+location={userData.location}
+avatar={userData.avatar}
+stats={userData.stats}
+    />
+    <FriendList friends={friends}
+    
+    />
+
+    <TransactionHistory items={transactions}/>
+  </>
+  
+  
+
+);
+};
+
+
+
+
+
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,34 @@
+export default function Profile({ avatar, username, tag, location, stats }) {
+    return (
+<div>
+<div>
+                <img
+                    src={avatar}
+alt="User avatar"/>
+                <p>{username}</p>
+                <p>@{tag}</p>
+                <p>{location}</p>
+</div>
+
+<ul>
+<li>
+<span>Followers</span>
+                    <span>{stats.followers}</span>
+</li>
+<li>
+<span>Views</span>
+                    <span>{ stats.views}</span>
+</li>
+<li>
+<span>Likes</span>
+                    <span>{stats.likes}</span>
+</li>
+</ul>
+</div>
+
+)
+}
+
+
+
+
